39135a06d2d6f6331428dc6de3194932e0ff80da
Ali Samad <aliussamad2004@gmail.com>
2025-08-09 16:51:12 +0530

    Power of Two

--- Leetcode_Practice_Questions/LC_215.cpp
+++ Leetcode_Practice_Questions/LC_215.cpp
@@ -1,8 +1,8 @@
 #include<iostream>
-#include<queue>
 #include<vector>
+#include<queue>
 using namespace std;
-int findKthLargest(vector<int>& nums, int k){
+int findkthLargestElement(vector<int>& nums, int k){
     priority_queue<int, vector<int>, greater<int>> minheap;
     for(int num : nums){
         minheap.push(num);
@@ -10,12 +10,21 @@ int findKthLargest(vector<int>& nums, int k){
             minheap.pop();
         }
     }
-    return minheap.top();
+    minheap.top();
 }
 int main(){
-    vector<int> nums = {3,2,1,5,6,4};
-    int k = 2;
-    int result = findKthLargest(nums, k);
-    cout << "The Kth largets element is " << result << endl;
+    int n;
+    int k;
+    cout << "Enter the value of N: " << endl;
+    cin >> n;
+    vector<int> nums(n);
+    cout << "Enter the elements of the array: " << endl;
+    for(int i = 0; i < n; i++){
+        cin >> nums[i];
+    }
+    cout << "Enter the value of K: " << endl;
+    cin >> k;
+    int result = findkthLargestElement(nums, k);
+    cout << "The " << k << "th Largest element in the array is " << result << endl;
     return 0;
 }
--- Leetcode_Practice_Questions/LC_231.cpp
+++ Leetcode_Practice_Questions/LC_231.cpp
@@ -1,9 +1,23 @@
-bool isPowerOfTwo(int n) {
+#include<iostream>
+using namespace std;
+bool isPowerofTwo(int n){
     if(n == 1){
         return true;
     }
-    if(n<=0 || n%2!=0){
+    if( n <=  0 || n % 2 != 0){
         return false;
     }
-    return isPowerOfTwo(n/2);
+    return isPowerofTwo(n/2);
+}
+int main(){
+    int n;
+    cout << "Enter the value of n: " << endl;
+    cin >> n;
+    if(isPowerofTwo(n)){
+        cout << "True" << endl;
+    }
+    else{
+        cout << "False" << endl;
+    }
+    return 0;
 }
